c22e3a1dd8f12e1bbbabe828938640e68d9ee1e3
JasterV <victorcoder2@gmail.com>
2021-07-24 19:01:58 +0200

    upload dataset improve

--- scripts/upload_dataset.js
+++ scripts/upload_dataset.js
@@ -3,36 +3,43 @@ const serviceAccount = require("../.firebase.json");
 const fs = require('fs');
 const readline = require('readline');
 
-const DATA_FILE_PATH = 'scripts/sentences.jsonl.txt';
+const args = process.argv.slice(2);
 
-(async () => {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
+if (args.length != 1) {
+    console.log('Dataset path required: ')
+    console.log('nodejs upload_dataset.js <dataset-path>')
+    process.exit(-1)
+}
 
-    const db = admin.firestore()
+const dataFilePath = args[0]
+    (async () => {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
 
-    console.log('Firestore credentials ok')
+        const db = admin.firestore()
 
-    const rl = readline.createInterface({
-        input: fs.createReadStream(DATA_FILE_PATH),
-        output: process.stdout,
-        terminal: false
-    });
+        console.log('Firestore credentials ok')
 
-    console.log('Reading sentences.jsonl.txt')
-    console.log('Importing data to firebase...')
+        const rl = readline.createInterface({
+            input: fs.createReadStream(dataFilePath),
+            output: process.stdout,
+            terminal: false
+        });
 
-    rl.on('line', async (line) => {
-        const sentence = JSON.parse(line)
-        const entry = Object.entries(sentence.cats).find((elem) => elem['1'] == 1)
-        if(!entry) {
-            console.log('Sentence: ', sentence, ' does not have a category')
-            return
-        }
-        await db.collection('sentences').doc().set({
-            text: sentence.text,
-            category: entry[0]
-        })
-    });
-})();
+        console.log('Reading sentences.jsonl.txt')
+        console.log('Importing data to firebase...')
+
+        rl.on('line', async (line) => {
+            const sentence = JSON.parse(line)
+            const entry = Object.entries(sentence.cats).find((elem) => elem['1'] == 1)
+            if (!entry) {
+                console.log('Sentence: ', sentence, ' does not have a category')
+                return
+            }
+            await db.collection('sentences').doc().set({
+                text: sentence.text,
+                category: entry[0]
+            })
+        });
+    })();
