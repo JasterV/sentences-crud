bc007024d7f23f58f329a71457c89bef7c6d595c
JasterV <victorcoder2@gmail.com>
2021-07-25 00:11:58 +0200

    views almost done

--- src/components/view/router.ts
+++ src/components/view/router.ts
@@ -11,7 +11,19 @@ const model = sentenceModel(db)
 router.get('/sentences', async (req, res) => {
     const { orderBy, order, lastId } = req.query
     const sentences: Sentence[] = await model.list({ orderBy, order, last: lastId })
-    return res.render('list', { sentences })
+    return res.render('list', { sentences, orderBy: orderBy ?? null, order })
+})
+
+router.get('/sentence/create', async (req, res) => {
+    const { text, category } = req.body
+    const id = await model.create({ text, category })
+    return res.redirect('/public/sentence/' + id)
+})
+
+router.get('/sentence/:id/delete', async (req, res) => {
+    const { id } = req.params
+    await model.del(id)
+    return res.redirect('/public/sentences')
 })
 
 router.get('/sentence/:id/update', async (req, res) => {
@@ -21,10 +33,10 @@ router.get('/sentence/:id/update', async (req, res) => {
     return res.render('sentence', { sentence })
 })
 
-router.get('/sentence/:id/delete', async (req, res) => {
+router.get('/sentence/:id/edit', async (req, res) => {
     const { id } = req.params
-    await model.del(id)
-    return res.redirect('/sentences')
+    const sentence: Sentence = await model.getById(id)
+    return res.render('form_sentence', { sentence })
 })
 
 router.get('/sentence/:id', async (req, res) => {
@@ -33,11 +45,7 @@ router.get('/sentence/:id', async (req, res) => {
     return res.render('sentence', { sentence })
 })
 
-router.get('/sentence', async (req, res) => {
-    const { text, category } = req.body
-    const id = await model.create({ text, category })
-    return res.redirect('/sentence/' + id)
-})
+router.get('/sentence', (_req, res) => res.render('form_sentence', { sentence: {} }))
 
 // EJS views
 router.get('/', (_req, res) => res.render('index'))
